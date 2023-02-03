efac2a86c378f14f87259e43a480e486a53515f7
FrancescoSquitieri <101020234+FrancescoSquitieri@users.noreply.github.com>
2023-02-03 12:47:44 +0100

    prima bozza

--- src/MUIslider/MuiSlider.tsx
+++ src/MUIslider/MuiSlider.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
-import {Grid, Slider} from "@mui/material";
+import {Box, Grid, Slider} from "@mui/material";
 import {MuiSliderProps, MuiSliderState} from "./MuiSlider.types";
+
+declare global {
+    interface Array<T> {
+        equals(array: Array<any>): boolean;
+    }
+}
+
+Array.prototype.equals = function (array: Array<any>) {
+    if (!array)
+        return false;
+    if(array === this)
+        return true;
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            if (!this[i].equals(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 
 class MuiSlider extends React.Component<MuiSliderProps, MuiSliderState> {
     constructor(props: MuiSliderProps) {
@@ -20,28 +47,40 @@ class MuiSlider extends React.Component<MuiSliderProps, MuiSliderState> {
                     label: '75%',
                     value: 75
                 }
-            ]
+            ],
+            sliderValue: [25, 50, 75]
         }
     }
 
     handleChangeSlider = (_: Event, value: number | number[]) => {
         console.log(value);
+        if (!(this.state.sliderValue as Array<number>).equals(value as number[])) {
+            console.log('non uguali');
+            this.setState(prevState => ({
+                ...prevState,
+                sliderValue: value
+            }))
+        }
     }
 
     render() {
         return (
             <>
-                <Grid>
-                    ciao
-                </Grid>
-
-                <Slider
-                    track={false}
-                    aria-labelledby="track-false-range-slider"
-                    defaultValue={[25, 50, 75]}
-                    onChange={this.handleChangeSlider}
-                    marks={this.state.marks}
-                />
+                <Box
+                    height='100vh'
+                    width='100%'
+                    display='flex'
+                    alignItems='center'
+                    justifyContent='center'
+                >
+                    <Slider
+                        track={false}
+                        aria-labelledby="track-false-range-slider"
+                        defaultValue={[25, 50, 75]}
+                        onChange={this.handleChangeSlider}
+                        marks={this.state.marks}
+                    />
+                </Box>
             </>
         )
     }
--- src/MUIslider/MuiSlider.types.ts
+++ src/MUIslider/MuiSlider.types.ts
@@ -8,5 +8,6 @@ export interface MuiSliderState {
     marks: {
         value: number,
         label: string;
-    }[]
+    }[];
+    sliderValue: number | number[];
 }
